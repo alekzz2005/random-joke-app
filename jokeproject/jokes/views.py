50bfa926d5a374cdaef930bdd500b37ca1ad36ab
alekzz2005 <binagatanalexander2005@gmail.com>
2025-09-22 16:29:26 +0800

    feature(jokes): complete app setup

--- jokeproject/jokes/views.py
+++ jokeproject/jokes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
+# Temporary joke list for testing (no DB yet)
+JOKES = [
+    "Why don’t programmers like nature? It has too many bugs.",
+    "Why do Python developers wear glasses? Because they don’t C#.",
+    "How many programmers does it take to change a light bulb? None, that’s a hardware problem!",
+    "Debugging is like being the detective in a crime movie where you are also the murderer."
+]
+
+def home(request):
+    joke = random.choice(JOKES)  # Pick a random joke
+    return render(request, 'jokes/home.html', {'joke': joke})
