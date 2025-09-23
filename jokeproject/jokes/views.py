f02ec44cf2aa5e9eb9748b82740e35c4bacb53c9
alekzz2005 <binagatanalexander2005@gmail.com>
2025-09-23 11:22:34 +0800

    feat(app): update views, template, and CSS styling

--- jokeproject/jokes/views.py
+++ jokeproject/jokes/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 import random
 
-# Temporary joke list for testing (no DB yet)
+# Expanded joke list
 JOKES = [
     "Why don’t programmers like nature? It has too many bugs.",
     "Why do Python developers wear glasses? Because they don’t C#.",
     "How many programmers does it take to change a light bulb? None, that’s a hardware problem!",
-    "Debugging is like being the detective in a crime movie where you are also the murderer."
+    "Debugging is like being the detective in a crime movie where you are also the murderer.",
+    "A SQL query walks into a bar, approaches two tables and asks: 'May I join you?'",
+    "There are only 10 kinds of people in this world: those who know binary and those who don’t.",
+    "I told my computer I needed a break, and it said 'No problem, I’ll go to sleep.'",
+    "Why was the developer broke? Because he used up all his cache."
 ]
 
 def home(request):
